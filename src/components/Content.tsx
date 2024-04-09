70d9764f38067443a5d052d91399debcc8de2154
MasnunMuhaemin <108683368+MasnunMuhaemin@users.noreply.github.com>
2024-04-09 22:20:54 +0700

    fix responsive pada componet content ukuran mobile

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -2,14 +2,14 @@ const Content = () => {
   return (
     <div className="bg-slate-300 rounded-md m-4 md:m-8 md:w-[875px] md:mx-auto">
       <div className="grid md:grid-cols-2">
-        <div className="ml-28 mt-5 mb-5 md:ml-24 md:mt-4 md:mb-4 bg-white h-[200px] w-[155px] md:h-[335px] md:w-[275px] lg:h-[385px] lg:w-[300px] rounded-xl shadow-2xl">
+        <div className="ml-[120px] mt-5 mb-5 md:ml-24 md:mt-4 md:mb-4 bg-white h-[200px] w-[155px] md:h-[335px] md:w-[275px] lg:h-[385px] lg:w-[300px] rounded-xl shadow-2xl">
           <img
             src="/images/wedding/wed-5.jpg"
             alt=""
-            className="h-[175px] md:h-[315px] lg:h-[350px] ml-3 md:ml-5 mt-3 md:mt-3 object-cover rounded-lg shadow-inner"
+            className="h-[175px] md:h-[315px] lg:h-[350px] ml-3 md:ml-5 mt-3 sm:mt-3 md:mt-4 object-cover rounded-lg shadow-inner"
           />
         </div>
-        <div className="md:mt-32 text-center">
+        <div className="md:mt-32  text-center">
           <h1 className="text-3xl md:text-4xl font-extrabold font-satisfy mb-3 tracking-widest">
             Akad Nikah
           </h1>
@@ -24,7 +24,7 @@ const Content = () => {
           </p>
         </div>
       </div>
-      <div className="w-[250px] md:w-[575px] text-center ml-16 md:ml-36 mt-2">
+      <div className="w-[275px] md:w-[575px] text-center ml-16 md:ml-36 mt-2">
         <p className="font-raleway">
           Merupakan suatu kehormatan dan kebahagiaan bagi kami apabila
           Bapak/Ibu/Saudara/i berkenan hadir untuk memberikan doa restu kepada
