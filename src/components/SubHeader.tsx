32e573a5e18a57c1207cfc562cdaf777afe5e8f1
MasnunMuhaemin <108683368+MasnunMuhaemin@users.noreply.github.com>
2024-04-09 13:44:52 +0700

    fix component subheader

--- src/components/SubHeader.tsx
+++ src/components/SubHeader.tsx
@@ -1,57 +1,39 @@
 const SubHeader = () => {
   return (
-    <div className="bg-slate-300 rounded-lg mt-3">
-      <div className="text-center mb-5">
-        <h1 className="text-2xl font-medium font-['playfair']">
-          Mempelai Pria & Wanita
+    <div className="bg-slate-300 rounded-lg mt-3 shadow-2xl">
+      <div className="text-center m-5">
+        <h1 className="text-2xl font-medium font-playfair">
+          Mempelai Pria <span className="font-raleway text-2xl font-medium">&</span> Wanita
         </h1>
       </div>
       <div className="grid grid-cols-3 gap-10 px-20">
         <div className="cursor-pointer">
           <img
             src="/images/gallery/image2.jpg"
-            alt="makanan1"
-            className="h-[250px] object-cover w-full rounded"
+            alt="wedding1"
+            className="h-[250px] object-cover w-full rounded shadow-xl"
           />
         </div>
         <div className="cursor-pointer">
           <img
             src="/images/gallery/image1.jpg"
-            alt="makanan2"
-            className="h-[250px] object-cover w-full rounded"
+            alt="wedding2"
+            className="h-[250px] object-cover w-full rounded shadow-xl"
           />
         </div>
         <div className="cursor-pointer">
           <img
             src="/images/gallery/image3.jpg"
-            alt="makanan2"
-            className="h-[250px] object-cover w-full rounded"
+            alt="wedding3"
+            className="h-[250px] object-cover w-full rounded shadow-xl"
           />
         </div>
       </div>
-      {/* <div className="grid grid-cols-4 teks-center">
-        <div className="cols-span-2">
-          <div className="bg-slate-500">
-            <h1>Ade &</h1>
-          </div>
-          <div className="bg-slate-500">
-            <h1>Mempelai Orang tua</h1>
-          </div>
-        </div>
-        <div className="cols-span-2">
-          <div className="bg-slate-500">
-            <p>Robi</p>
-          </div>
-          <div className="bg-slate-500">
-            <p>Mempelai orang tua</p>
-          </div>
-        </div>
-      </div> */}
       <div className="flex flex-col">
-        <div className="inline-block text-center mt-5">
+        <div className="inline-block text-center text-3xl font-satisfy mt-5">
             <p>Ade & Robi</p>
         </div>
-        <div className="inline-block text-center">
+        <div className="inline-block text-center text-base font-medium font-raleway">
             <p>Putri Pertama Bapak Elon Dahlan & Ibu Uhe, Putra Kedua Bapak Syahromi & Ibu Anah</p>
         </div>
       </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,18 @@
 import invitations from '../lib/data.json'
 import NotFound from './404'
+import Content from '../components/Content'
 
 const Home = () => {
     const urlSearch = new URLSearchParams(window.location.search)
     const name = urlSearch.get('name')
 
     if (!name) return <NotFound />
-
-    if (!invitations.some(people => people.name === name)) return <NotFound />
-
+    
+    if (!invitations.some(people => people.name === name)) return (
+        <div className="">You has not invited!</div>
+    )
     return (
-        <div className=""></div>
+        <Content></Content>
     )
     
 }
