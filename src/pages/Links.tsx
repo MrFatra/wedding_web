07d2b11df6522b53bf33b8704986271d8d32ff1d
MrFatra <ramadhanfatra2004@gmail.com>
2024-04-10 21:43:36 +0700

    style: links grid

--- src/pages/Links.tsx
+++ src/pages/Links.tsx
@@ -3,13 +3,20 @@ import { Link } from 'react-router-dom'
 
 const Links = () => {
   return (
-    invitations.map((item, index) => {
-      return (
-        <div className="">
-          <Link to={`/invite?name=${item.name}`} key={index}>Undangan untuk {item.name}</Link>
-        </div>
-      )
-    })
+    <div className="grid grid-cols-2 gap-6 md:grid-cols-3 lg:grid-cols-4 text-white bg-slate-500 w-full p-5">
+      {
+        invitations.map((item, index) => {
+          return (
+            <Link
+              key={index}
+              to={`/invite?name=${item.name}`}
+              className="flex justify-center items-center shadow-lg rounded-lg backdrop-blur-sm bg-white/30 border py-2 px-3 text-sm sm:text-base">
+              Undangan untuk {item.name}
+            </Link>
+          )
+        })
+      }
+    </div>
   )
 }
 
