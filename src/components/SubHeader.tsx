8378403ad40b0d19489ad58dacea1ddf4a8f35b3
MasnunMuhaemin <108683368+MasnunMuhaemin@users.noreply.github.com>
2024-04-11 14:06:22 +0700

    animation on component content and subheader

--- src/components/SubHeader.tsx
+++ src/components/SubHeader.tsx
@@ -1,7 +1,7 @@
 const SubHeader = () => {
   return (
     <div className="bg-orange-100 rounded-lg mt-3 shadow-2xl md:m-6 p-4 md:p-8 text-gray-500">
-      <div className="text-center m-5">
+      <div data-aos="fade-up" data-aos-duration="2000" className="text-center m-5">
         <h1 className="text-2xl md:text-3xl font-medium font-playfair">
           Mempelai Pria{" "}
           <span className="font-raleway text-2xl md:text-3xl font-medium">
@@ -15,6 +15,8 @@ const SubHeader = () => {
           <img
             src="/images/wedding/wed-1.jpg"
             alt="wedding1"
+            data-aos="fade-up"
+            data-aos-duration="2000"
             className="h-[435px] md:h-[200px] lg:h-[485px] object-cover w-full rounded shadow-2xl"
           />
         </div>
@@ -22,6 +24,8 @@ const SubHeader = () => {
           <img
             src="/images/wedding/wed-3.jpg"
             alt="wedding2"
+            data-aos="fade-up"
+            data-aos-duration="3000"
             className="h-[175px] md:h-[200px] lg:h-[485px]  object-cover w-full rounded shadow-2xl"
           />
         </div>
@@ -29,11 +33,17 @@ const SubHeader = () => {
           <img
             src="/images/wedding/wed-8.jpg"
             alt="wedding3"
+            data-aos="fade-up"
+            data-aos-duration="3000"
             className="h-[435px] md:h-[200px] lg:h-[485px] object-cover w-full rounded shadow-2xl"
           />
         </div>
       </div>
-      <div className="flex flex-col mt-4 md:mt-8">
+      <div
+        data-aos="fade-up"
+        data-aos-duration="3000"
+        className="flex flex-col mt-4 md:mt-8"
+      >
         <div className="inline-block text-center text-xl md:text-3xl font-satisfy">
           <p>Ade Irna Rohmatillah & Muhammad Ilyas Robiansyah</p>
         </div>
