f8764069a0ba75c3980bcafe744f2abef6f37ab0
MrFatra <ramadhanfatra2004@gmail.com>
2024-04-11 19:59:28 +0700

    feat: add animation

--- src/components/Quotes.tsx
+++ src/components/Quotes.tsx
@@ -1,6 +1,6 @@
 const Quotes = () => {
     return (
-        <div className="mx-8 mb-10 bg-orange-100 p-10 rounded-lg text-gray-500">
+        <div className="mx-8 mb-10 bg-orange-100 p-10 rounded-lg text-gray-500" data-aos="fade" data-aos-duration="2000" data-aos-offset="500">
             <div className="flex items-center justify-center mb-10">
                 <svg width="24px" height="24px" viewBox="0 0 24 24" fill="#66474a" xmlns="http://www.w3.org/2000/svg">
                     <path d="M11.5757 1.42426C11.81 1.18995 12.1899 1.18995 12.4243 1.42426L22.5757 11.5757C22.81 11.81 22.8101 12.1899 22.5757 12.4243L12.4243 22.5757C12.19 22.81 11.8101 22.8101 11.5757 22.5757L1.42426 12.4243C1.18995 12.19 1.18995 11.8101 1.42426 11.5757L11.5757 1.42426Z" />
