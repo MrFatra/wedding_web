04ebc49e393011005e24654524a63e23c47fb14d
MrFatra <ramadhanfatra2004@gmail.com>
2024-04-12 23:26:22 +0700

    perf: change countdown package

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,7 +1,7 @@
-import Countdown from 'react-countdown';
+import Countdown from 'react-countdown-simple';
 
 const TwoDaysCountdown = () => {
-    let targetDate = new Date('2024-04-18T08:00:00+07:00');
+    let targetDate = new Date('2024-04-18T08:00:00+07:00').toISOString();
 
     return (
         <div className="w-full h-screen bg-[url('/images/wedding/wed-4.jpg')] lg:bg-orange-100 sm:bg-orange-200 md:bg-none bg-cover flex flex-col items-center justify-center relative">
@@ -11,38 +11,31 @@ const TwoDaysCountdown = () => {
                     <div className="h-[2.5px] bg-gray-500 rounded-full"></div>
                 </div>
                 <Countdown
-                    date={targetDate}
-                    onComplete={() => {
-                        console.log('Countdown selesai');
-                    }}
-                    renderer={({ days, hours, minutes, seconds, completed }) => {
-                        if (completed) {
-                            return <span className="">Acara sedang berlangsung</span>;
-                        } else {
-                            return (
-                                <div className="font-reddit flex flex-wrap justify-center items-center lg:gap-10 sm:gap-10 gap-7">
-                                    <div className="text-center">
-                                        <span className="text-lg md:text-2xl font-bold">{days}</span><br />
-                                        <span className="">Hari</span>
-                                    </div>
-                                    <div className="text-center">:</div>
-                                    <div className="text-center">
-                                        <span className="text-lg md:text-2xl font-bold">{hours}</span><br />
-                                        <span className="">Jam</span>
-                                    </div>
-                                    <div className="text-center">:</div>
-                                    <div className="text-center">
-                                        <span className="text-lg md:text-2xl font-bold">{minutes}</span><br />
-                                        <span className="">Menit</span>
-                                    </div>
-                                    <div className="text-center">:</div>
-                                    <div className="text-center">
-                                        <span className="text-lg md:text-2xl font-bold">{seconds}</span><br />
-                                        <span className="">Detik</span>
-                                    </div>
+                    targetDate={targetDate}
+                    renderer={({ days, hours, minutes, seconds }) => {
+                        return (
+                            <div className="font-reddit text-gray-500 flex flex-wrap justify-center items-center lg:gap-10 sm:gap-10 gap-7">
+                                <div className="text-center">
+                                    <span className="text-lg md:text-2xl font-bold">{days}</span><br />
+                                    <span className="">Hari</span>
                                 </div>
-                            );
-                        }
+                                <div className="text-center">:</div>
+                                <div className="text-center">
+                                    <span className="text-lg md:text-2xl font-bold">{hours}</span><br />
+                                    <span className="">Jam</span>
+                                </div>
+                                <div className="text-center">:</div>
+                                <div className="text-center">
+                                    <span className="text-lg md:text-2xl font-bold">{minutes}</span><br />
+                                    <span className="">Menit</span>
+                                </div>
+                                <div className="text-center">:</div>
+                                <div className="text-center">
+                                    <span className="text-lg md:text-2xl font-bold">{seconds}</span><br />
+                                    <span className="">Detik</span>
+                                </div>
+                            </div>
+                        )
                     }}
                 />
             </div>
